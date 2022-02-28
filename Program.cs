158b7ef6b64113a5b25d62c6656ef4c7432f22bb
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-02-28 14:26:08 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Guess the secret number... uwu?");
+          int x = int.Parse(Console.ReadLine());
+           Console.WriteLine($"your guess is {x}");
+           int secretNumber = 42;
+           Console.Clear();
+        }
+    }
+}
