7abe0e9226c7e0b0270e5a1dfa067c2b47480b0a
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-02-28 15:28:44 -0500

    add difficulty levels

--- Program.cs
+++ Program.cs
@@ -8,14 +8,32 @@ namespace guessingGame
         static void Main(string[] args)
         {
             int secretNumber = new Random().Next(100);
-            
-          int guessCounter =1;
-          while (guessCounter < 5)
+            int guessCounter =1;
+            int totalGuesses;
+
+          Console.WriteLine("What difficulty would you like: \n1=easy(8 trys), \n2=medium(6 trys), \n3=hard(4 trys)");  
+          int diff = int.Parse(Console.ReadLine());
+
+          if (diff == 1)
+          {
+              totalGuesses =8;
+          }
+          else if (diff == 2)
+          {
+              totalGuesses=6;
+          }
+          else
+          {
+              totalGuesses=4;
+          }
+
+         
+          while (guessCounter < totalGuesses + 1)
           {
 
           Console.WriteLine("Guess the secret number... uwu?");
           Console.WriteLine($"This is guess #{guessCounter}");
-          Console.WriteLine($"You have {4-(guessCounter-1)} guesses left");
+          Console.WriteLine($"You have {totalGuesses-(guessCounter-1)} guesses left");
           int guess = int.Parse(Console.ReadLine());
            
            
